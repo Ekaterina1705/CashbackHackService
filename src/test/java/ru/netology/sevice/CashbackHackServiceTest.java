560ca344274bc0a1bda734a8f562abce5fdc33aa
Neto test <foxekaterina@yandex.ru>
2023-03-04 09:54:21 +0900

    Added CashbackHackService

--- src/test/java/ru/netology/sevice/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/sevice/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.sevice;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
 class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
